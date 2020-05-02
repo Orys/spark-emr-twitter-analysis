19190c98a3f1b3cdc5ae6fcac9e4891f44b3b6ca
giuliofortini <giulio.forty@gmail.com>
2020-05-02 18:10:41 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,30 @@
+name := "spark-emr-twitter-analysis"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
+
+
+val sparkVersion = "2.4.5"
+
+resolvers ++= Seq(
+  "bintray-spark-packages" at "https://dl.bintray.com/spark-packages/maven",
+  "Typesafe Simple Repository" at "https://repo.typesafe.com/typesafe/simple/maven-releases",
+  "MavenRepository" at "https://mvnrepository.com"
+)
+
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.4",
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.4" classifier "models",
+  "edu.stanford.nlp" % "stanford-parser" % "3.4"
+
+  // logging
+  //"org.apache.logging.log4j" % "log4j-api" % "2.4.1",
+  //"org.apache.logging.log4j" % "log4j-core" % "2.4.1",
+  // postgres for DB connectivity
+  //"org.postgresql" % "postgresql" % postgresVersion
+
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,130 @@
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{BooleanType, DateType, IntegerType, StringType, StructField, StructType}
+
+object Main {
+
+  // define case class for data
+  case class Tweet(
+                    user_id: String,
+                    screen_name: String,
+                    verified: Boolean,
+                    country: String,
+                    source: String,
+                    followers_count: Int,
+                    friends_count: Int,
+                    favourites_count: Int,
+                    retweet_count: Int,
+                    created_at: String,
+                    text: String,
+                    lang: String
+                  )
+
+
+  def main(args: Array[String]) {
+    /**
+     * This creates a SparkSession, which will be used to operate on the DataFrames that we create.
+     */
+    val spark = SparkSession.builder()
+      .appName("spark-project")
+      //.config("spark.master", "local")
+      .getOrCreate()
+
+    /**
+     * The SparkContext (usually denoted `sc` in code) is the entry point for low-level Spark APIs, including access to Resilient Distributed Datasets (RDDs).
+     */
+    val sc = spark.sparkContext
+
+    import spark.implicits._
+
+    // defining the schemas
+    val countriesSchema = StructType(
+      Array(
+        StructField("country", StringType),
+        StructField("country_code", StringType)
+      )
+    )
+
+    val tweetsSchema = StructType(
+      Array(
+        StructField("status_id", StringType),
+        StructField("user_id", StringType),
+        StructField("created_at", DateType),
+        StructField("screen_name", StringType),
+        StructField("text", StringType),
+        StructField("source", StringType),
+        StructField("reply_to_status_id", StringType),
+        StructField("reply_to_user_id", StringType),
+        StructField("reply_to_screen_name", StringType),
+        StructField("is_quote", BooleanType),
+        StructField("is_retweet", BooleanType),
+        StructField("favourites_count", IntegerType),
+        StructField("retweet_count", IntegerType),
+        StructField("country_code", StringType),
+        StructField("place_full_name", StringType),
+        StructField("place_type", StringType),
+        StructField("followers_count", IntegerType),
+        StructField("friends_count", IntegerType),
+        StructField("account_lang", StringType),
+        StructField("account_created_at", DateType),
+        StructField("verified", BooleanType),
+        StructField("lang", StringType)
+      )
+    )
+
+
+    val countriesDF = spark.read
+      .option("header", "true")
+      .schema(countriesSchema)
+      .csv("s3n://spark-project-test/twitter-data/Countries.CSV")
+
+
+    val tweetsDF = spark.read
+      .option("header", "true")
+      .schema(tweetsSchema)
+      .option("dateFormat", "yyyy-MM-dd'T'HH:mm:ss'Z'")
+      .csv("s3n://spark-project-test/twitter-data/2020*.CSV")
+      .select(
+        "user_id",
+        "screen_name",
+        "verified",
+        "country_code",
+        "source",
+        "followers_count",
+        "friends_count",
+        "favourites_count",
+        "retweet_count",
+        "created_at",
+        "text",
+        "lang"
+      )
+
+
+
+    val cleanDF = tweetsDF
+      .where(col("text").isNotNull
+        and col("created_at").isNotNull
+        and col("country_code") === "US"
+        and col("lang") === "en"
+      )
+      .join(countriesDF, tweetsDF.col("country_code") === countriesDF.col("country_code")).drop("country_code")
+
+    println(tweetsDF.count())
+
+    cleanDF.show()
+
+    // most active users
+    cleanDF.groupBy("screen_name").agg(
+      approx_count_distinct("text").as("nTweets")
+    ).orderBy(desc("nTweets")).show
+
+
+    tweetsDF.sort(desc("followers_count")).limit(20).show()
+
+    cleanDF.select("screen_name", "text", "retweet_count", "favourites_count").orderBy(desc("retweet_count")).show
+
+    val tweetsRDD = cleanDF.as[Tweet].rdd
+
+    tweetsRDD.toDF.show()
+  }
+}
