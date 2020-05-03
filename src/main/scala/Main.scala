8175d690d2a56d5645e22ee8eb5c0c1fe24c9cda
Orys <aleliscio97@gmail.com>
2020-05-03 20:04:20 +0200

    writing data to parquet and added some functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,8 @@
+import DataManipulations._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-import DataManipulations._
+
 
 object Main {
 
@@ -18,27 +19,25 @@ object Main {
                     retweet_count: Int,
                     created_at: String,
                     text: String,
-                    lang: String
+                    lang: String,
+                    sentiment: Int
                   )
 
 
   def main(args: Array[String]) {
-    /**
-     * This creates a SparkSession, which will be used to operate on the DataFrames that we create.
-     */
+
+    // This creates a SparkSession, which will be used to operate on the DataFrames that we create.
     val spark = SparkSession.builder()
       .appName("spark-project")
       .config("spark.master", "local")
       .getOrCreate()
 
-    /**
-     * The SparkContext (usually denoted `sc` in code) is the entry point for low-level Spark APIs, including access to Resilient Distributed Datasets (RDDs).
-     */
+
+    //The SparkContext (usually denoted `sc` in code) is the entry point for low-level Spark APIs, including access to Resilient Distributed Datasets (RDDs).
     val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
 
-    import spark.implicits._
-
-    // defining the schemas
+    // defining the countries schema
     val countriesSchema = StructType(
       Array(
         StructField("country", StringType),
@@ -46,48 +45,53 @@ object Main {
       )
     )
 
+    // defining the tweets schema
     val tweetsSchema = StructType(
       Array(
         StructField("status_id", StringType),
         StructField("user_id", StringType),
-        StructField("created_at", DateType),
+        StructField("created_at", StringType, true),
         StructField("screen_name", StringType),
         StructField("text", StringType),
         StructField("source", StringType),
         StructField("reply_to_status_id", StringType),
         StructField("reply_to_user_id", StringType),
         StructField("reply_to_screen_name", StringType),
-        StructField("is_quote", BooleanType),
-        StructField("is_retweet", BooleanType),
-        StructField("favourites_count", IntegerType),
-        StructField("retweet_count", IntegerType),
+        StructField("is_quote", BooleanType, true),
+        StructField("is_retweet", BooleanType, true),
+        StructField("favourites_count", IntegerType, true),
+        StructField("retweet_count", IntegerType, true),
         StructField("country_code", StringType),
         StructField("place_full_name", StringType),
         StructField("place_type", StringType),
-        StructField("followers_count", IntegerType),
-        StructField("friends_count", IntegerType),
+        StructField("followers_count", IntegerType, true),
+        StructField("friends_count", IntegerType, true),
         StructField("account_lang", StringType),
-        StructField("account_created_at", DateType),
-        StructField("verified", BooleanType),
-        StructField("lang", StringType)
+        StructField("account_created_at", StringType, true),
+        StructField("verified", BooleanType, true),
+        StructField("lang", StringType),
+        StructField("sentiment", IntegerType, true)
       )
     )
 
-
+    // read countries data as DataFrame
     val countriesDF = spark.read
       .option("header", "true")
       .schema(countriesSchema)
       .csv("src/main/resources/data/Countries.csv")
       //.csv("s3n://spark-project-test/twitter-data/Countries.CSV")
 
-
+    // read tweets data as DataFrame and select useful columns
     val tweetsDF = spark.read
-      .option("header", "true")
+      //.option("inferSchema", "true")
       .schema(tweetsSchema)
+      //.option("header", "true")
       .option("dateFormat", "yyyy-MM-dd'T'HH:mm:ss'Z'")
+      .parquet("src/main/resources/data/withSentiment.parquet")
+
       //.csv("src/main/resources/data/2020*.csv")
       //.csv("s3n://spark-project-test/twitter-data/2020*.CSV")
-      .csv("src/main/resources/data/2020-03-12_Covid_Tweets.csv")
+      //.csv("src/main/resources/data/2020-03-00_Covid_Tweets.csv")
       .select(
         "user_id",
         "screen_name",
@@ -100,34 +104,66 @@ object Main {
         "retweet_count",
         "created_at",
         "text",
-        "lang"
+        "lang",
+        "sentiment"
       )
+    //tweetsDF.printSchema()
+    println("Rows in dataset: " + tweetsDF.count())
+    tweetsDF.show()
 
+
+    // clean DataFrame
     val cleanDF = tweetsDF
-      .where(col("text").isNotNull
-        and col("created_at").isNotNull
+      .where(col("lang") === "en"
         and col("country_code") === "US"
-        and col("lang") === "en"
+        and col("created_at").isNotNull
+        and col("followers_count").isNotNull
+        and col("favourites_count").isNotNull
+        and col("retweet_count").isNotNull
       )
-      .join(countriesDF, tweetsDF.col("country_code") === countriesDF.col("country_code")).drop("country_code")
+      // change country_code with country by joining tweets and countries DataFrames
+      .join(countriesDF, tweetsDF.col("country_code") === countriesDF.col("country_code")).drop("country_code").cache()
 
-    println(tweetsDF.count())
-
+    println("Rows in dataset: " + cleanDF.count())
     cleanDF.show()
 
-    // most active users
-    cleanDF.groupBy("screen_name").agg(
-      approx_count_distinct("text").as("nTweets")
-    ).orderBy(desc("nTweets")).show
-
-    tweetsDF.sort(desc("followers_count")).limit(20).show()
-
+    // ordered by followers count
+    println("Ordered by followers count")
+    cleanDF.orderBy(desc("followers_count")).show()
+    // ordered by retweet_count
+    println("Ordered by retweet_count")
     cleanDF.select("screen_name", "text", "retweet_count", "favourites_count").orderBy(desc("retweet_count")).show
+    // ordered by favourites_count
+    println("Ordered by favourites_count")
     cleanDF.select("screen_name", "text", "retweet_count", "favourites_count").orderBy(desc("favourites_count")).show
 
-    val tweetsRDD = cleanDF.as[Tweet].rdd
+    // tweetsDF -> tweetsRDD
+    import spark.implicits._
+    val tweetsRDD = cleanDF.as[Tweet].rdd.cache()
 
-    val withSentimentRDD = withSentiment(tweetsRDD)
-    withSentimentRDD.toDF.show()
+    // show most active users
+    println("Most active users")
+    namesNTweetsFollowersFriends(tweetsRDD).toDF.show
+
+    // show countries with respective average sentiment
+    println("Country and average sentiment")
+    countryAvgSentiment(tweetsRDD).toDF.show()
+
+    // show sentiments with respective ratios followers/favourites and followers/retweets
+    println("Sentiment, followers/favourites, followers/retweets")
+    sentimentFavouritesRetweetsRatios(tweetsRDD).toDF.show()
+
+    // show the average sentiment for each day (created_at)
+    println("Average sentiment of the date")
+    avgSentimentDate(tweetsRDD).toDF.show()
+
+    // show sentiments with respective number of tweets
+    println("Number of tweets for each sentiment")
+    sentimentNTweets(tweetsRDD).toDF.show()
+
+    // show sentiments with respective number of tweets for each day (created_at)
+    println("Number of tweets for each sentiment for each date")
+    sentimentNTweetsDate(tweetsRDD).toDF.show()
+
   }
 }
--- src/main/scala/SentimentAnalyzer.scala
+++ src/main/scala/SentimentAnalyzer.scala
@@ -4,14 +4,170 @@ import edu.stanford.nlp.ling.CoreAnnotations
 import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
 import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types._
 
 import scala.collection.convert.wrapAll._
 
-object SentimentAnalyzer{
+object SentimentAnalyzer {
 
   val props = new Properties()
-  props.setProperty("annotators", "tokenize, ssplit, parse, sentiment")
+  props.setProperty("annotators", "tokenize, ssplit, pos,  parse, sentiment")
+  props.setProperty("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz")
   val pipeline: StanfordCoreNLP = new StanfordCoreNLP(props)
+
+  // define case class for data
+
+  case class Tweet(
+                    status_id: String,
+                    user_id: String,
+                    created_at: String,
+                    screen_name: String,
+                    text: String,
+                    source: String,
+                    reply_to_status_id: String,
+                    reply_to_user_id: String,
+                    reply_to_screen_name: String,
+                    is_quote: Option[Boolean],
+                    is_retweet: Option[Boolean],
+                    favourites_count: Option[Int],
+                    retweet_count: Option[Int],
+                    country_code: String,
+                    place_full_name: String,
+                    place_type: String,
+                    followers_count: Option[Int],
+                    friends_count: Option[Int],
+                    account_lang: String,
+                    account_created_at: String,
+                    verified: Option[Boolean],
+                    lang: String
+                  )
+
+  def main(args: Array[String]) {
+
+    // This creates a SparkSession, which will be used to operate on the DataFrames that we create.
+    val spark = SparkSession.builder()
+      .appName("sentiment-analyzer")
+      .config("spark.master", "local")
+      //.config("spark.sql.parquet.writeLegacyFormat" , "true")
+      .getOrCreate()
+
+
+    // The SparkContext (usually denoted `sc` in code) is the entry point for low-level Spark APIs, including access to Resilient Distributed Datasets (RDDs).
+    val sc = spark.sparkContext
+    //sc.setLogLevel("ERROR")
+
+    val tweetsSchema = StructType(
+      Array(
+        StructField("status_id", StringType),
+        StructField("user_id", StringType),
+        StructField("created_at", DateType),
+        StructField("screen_name", StringType),
+        StructField("text", StringType),
+        StructField("source", StringType),
+        StructField("reply_to_status_id", StringType),
+        StructField("reply_to_user_id", StringType),
+        StructField("reply_to_screen_name", StringType),
+        StructField("is_quote", BooleanType),
+        StructField("is_retweet", BooleanType),
+        StructField("favourites_count", IntegerType),
+        StructField("retweet_count", IntegerType),
+        StructField("country_code", StringType),
+        StructField("place_full_name", StringType),
+        StructField("place_type", StringType),
+        StructField("followers_count", IntegerType),
+        StructField("friends_count", IntegerType),
+        StructField("account_lang", StringType),
+        StructField("account_created_at", DateType),
+        StructField("verified", BooleanType),
+        StructField("lang", StringType)
+      )
+    )
+
+    val tweetsDF = spark.read
+      .option("header", "true")
+      .schema(tweetsSchema)
+      .option("dateFormat", "yyyy-MM-dd'T'HH:mm:ss'Z'")
+      //.csv("src/main/resources/data/2020*.csv")
+      .csv("src/main/resources/data/2020-03-00_Covid_Tweets.csv")
+      //.csv("s3n://spark-project-test/twitter-data/2020*.CSV")
+      //.csv("src/main/resources/data/2020-03-00_Covid_Tweets.csv")
+      .where(
+        col("text").isNotNull
+        and col("lang") === "en"
+        and col("country_code") === "US"
+        and col("created_at").isNotNull
+      )//.limit(100)
+
+    println("Rows in dataset: " + tweetsDF.count())
+    tweetsDF.show()
+
+    import spark.implicits._
+    val outFile = "src/main/resources/data/withSentiment.parquet"
+    val result = withSentiment(tweetsDF.as[Tweet].rdd)
+    result.toDF.write.mode(SaveMode.Overwrite).save(outFile)
+    result.toDF.show()
+    println("Data successfully written to: " + outFile)
+
+  }
+
+  /**
+   * add column with Sentiment Analysis results to data
+   */
+
+    case class WithSentiment(status_id: String,
+                             user_id: String,
+                             created_at: String,
+                             screen_name: String,
+                             text: String,
+                             source: String,
+                             reply_to_status_id: String,
+                             reply_to_user_id: String,
+                             reply_to_screen_name: String,
+                             is_quote: Option[Boolean],
+                             is_retweet: Option[Boolean],
+                             favourites_count: Option[Int],
+                             retweet_count: Option[Int],
+                             country_code: String,
+                             place_full_name: String,
+                             place_type: String,
+                             followers_count: Option[Int],
+                             friends_count: Option[Int],
+                             account_lang: String,
+                             account_created_at: String,
+                             verified: Option[Boolean],
+                             lang: String,
+                             sentiment: Int)
+  def withSentiment(dataRDD: RDD[Tweet]): RDD[WithSentiment] = dataRDD.map(
+    tweet => WithSentiment(
+      tweet.status_id,
+      tweet.user_id,
+      tweet.created_at,
+      tweet.screen_name,
+      tweet.text,
+      tweet.source,
+      tweet.reply_to_status_id,
+      tweet.reply_to_user_id,
+      tweet.reply_to_screen_name,
+      tweet.is_quote,
+      tweet.is_retweet,
+      tweet.favourites_count,
+      tweet.retweet_count,
+      tweet.country_code,
+      tweet.place_full_name,
+      tweet.place_type,
+      tweet.followers_count,
+      tweet.friends_count,
+      tweet.account_lang,
+      tweet.account_created_at,
+      tweet.verified,
+      tweet.lang,
+      mainSentiment(tweet.text)
+    )
+  )
+
 
   /**
    * Extracts the main sentiment for a given input
@@ -30,16 +186,20 @@ object SentimentAnalyzer{
   }
 
   private def extractSentiment(text: String): Int = {
-    val (_, sentiment) = extractSentiments(text)
-      .maxBy { case (sentence, _) => sentence.length }
-    sentiment
+    val sentiments = extractSentiments(text)
+    if (sentiments.isEmpty) 2
+    else {
+      val (_, sentiment) = extractSentiments(text)
+        .maxBy { case (sentence, _) => sentence.length }
+      sentiment
+    }
   }
 
   private def extractSentiments(text: String): List[(String, Int)] = {
     val annotation: Annotation = pipeline.process(text)
     val sentences = annotation.get(classOf[CoreAnnotations.SentencesAnnotation])
     sentences
-      .map(sentence => (sentence, sentence.get(classOf[SentimentCoreAnnotations.AnnotatedTree])))
+      .map(sentence => (sentence, sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])))
       .map { case (sentence, tree) => (sentence.toString, RNNCoreAnnotations.getPredictedClass(tree)) }
       .toList
   }
@@ -75,9 +235,6 @@ object Sentiment extends Enumeration {
     case POSITIVE => "POSITIVE"
     case VERY_POSITIVE => "VERY_POSITIVE"
   }
-
-
-
 }
 
  */
